e40dd40b10fa17777fcea7b61fc7d2a09bd94698
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-19 19:19:43 -0500

    nesting

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,24 @@ const { projects, clients } = require('../sampleData.js')
 
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql')
 
+//Client Type
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID},
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        client: {
+            type: ClientType,
+            resolve(parent, args){
+                return clients.find(client => client.id === parent.clientId)
+            }
+        }
+    }),
+})
+
+//Client Type
 const ClientType = new GraphQLObjectType({
     name: 'Client',
     fields: () => ({
@@ -15,6 +33,19 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects;
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: { id: {type: GraphQLID } },
+            resolve(parent, args){
+                return projects.find((project) => project.id === args.id)
+            }
+        },
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
