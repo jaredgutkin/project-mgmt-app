521469a5909a130d135a6abe231622c60bf4eccd
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-20 20:11:39 -0500

    add project

--- server/models/Project.js
+++ server/models/Project.js
@@ -9,7 +9,7 @@ const ProjectSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ['Not started', 'In progress', 'completed']
+        enum: ['Not Started', 'In Progress', 'Completed']
     },
     clientId: {
         type: mongoose.Schema.Types.ObjectId,
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,7 +9,8 @@ const {
     GraphQLString, 
     GraphQLSchema, 
     GraphQLList, 
-    GraphQLNonNull 
+    GraphQLNonNull,
+    GraphQLEnumType, 
 } = require('graphql')
 
 //Client Type
@@ -76,6 +77,7 @@ const RootQuery = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
     name: 'Mutation',
     fields: {
+        //Add client
         addClient: {
             type: ClientType,
             args: {
@@ -101,6 +103,37 @@ const mutation = new GraphQLObjectType({
             },
             resolve(parent, args) {
                 return Client.findByIdAndRemove(args.id)
+            }
+        },
+        //Add a project
+        addProject: {
+            type: ProjectType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString)},
+                description: { type: GraphQLNonNull(GraphQLString)},
+                status: {
+                    type: new GraphQLEnumType({
+                        name: 'ProjectStatus',
+                        values: {
+                            new: { value: 'Not Started' },
+                            progress: { value: 'In Progress' },
+                            completed: { value: 'Completed' },
+                        }
+                    }),
+                    defaultValue: 'Not Started',
+                },
+                clientId: { type: GraphQLNonNull(GraphQLID)}
+
+            },
+            resolve(parent, args) {
+                const project = new Project({
+                    name: args.name,
+                    description: args.description,
+                    status: args.status,
+                    clientId: args.clientId,
+                })
+
+                return project.save()
             }
         }
     }
